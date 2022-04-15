1a862d1e4cbc3fa173faf9d6f3cfdbd43542cf8e
XQ.Z <xxx@qq.com>
2022-04-15 20:02:31 +0800

    --测试

---
+++ Java_se_quize/src/main/java/com/test/concurrent/Test.java
@@ -0,0 +1,62 @@
+package com.test.concurrent;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Test
+ *
+ * @author XQ.Zhu
+ * @version 1.0
+ * 2022/4/12 17:45
+ **/
+public class Test extends Thread{
+    public static void main(String[] args) {
+//        String a="aa";
+//        String b="bb";
+//        String c=a+b;
+//        System.out.println(c==(a + b));
+//        System.out.println(Thread.currentThread().getName());
+//        Thread thread=new Test();
+//        thread.start();
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+//        new Thread(()->{
+//            for (int i = 0; i < 10; i++) {
+//                System.out.println(Thread.currentThread().getName()+":"+i*i);
+//
+//            }
+//        }).start();
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("x","test");
+        System.out.println(map.hashCode());
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName()+":"+i);
+        }
+    }
+}
+
+class LockTest{
+
+    public void testLock(){
+        ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+        Lock readLock = rwl.readLock();
+        readLock.lock();
+        try {
+
+        }finally {
+            readLock.unlock();
+        }
+    }
+}
---
+++ Java_se_quize/src/main/java/com/test/concurrent/Volatile.java
@@ -0,0 +1,33 @@
+package com.test.concurrent;
+
+/**
+ * Volatile
+ *
+ * @author XQ.Zhu
+ * @version 1.0
+ * 2022/4/14 22:19
+ **/
+public class Volatile implements Runnable {
+
+    private volatile static int a=0;
+
+    public static void main(String[] args) {
+        Volatile aVolatile = new Volatile();
+        Volatile bVolatile = new Volatile();
+        Thread thread = new Thread(aVolatile);
+        Thread thread1 = new Thread(bVolatile);
+        thread.start();
+        thread1.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            // 数字大一些，不然看不到效果
+            // 这里在a++前后打印变量值
+            System.out.println(Thread.currentThread().getId()+" before a = " + a);
+            a++;
+            System.out.println(Thread.currentThread().getId()+" after a = " + a);
+        }
+    }
+}
