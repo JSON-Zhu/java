8d179364ccb0a4656ed50be0e70131f6b27b8f14
XQ.Z <xxx@qq.com>
2022-04-18 10:59:09 +0800

    --test v2

--- Java_se_quize/src/main/java/com/test/concurrent/Volatile.java
+++ Java_se_quize/src/main/java/com/test/concurrent/Volatile.java
@@ -20,7 +20,7 @@ public class Volatile implements Runnable {
         thread1.start();
         thread.run();
         thread1.run();
-        
+        System.out.println("test");
     }
 
     @Override
